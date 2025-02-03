e0a870d99eae0ce487b7f8e2dc910d6863935e6c
Bektur13 <duyshembekov04@mail.ru>
2025-02-03 11:20:05 -0800

    Completed simple TCP server and client-server on Python

--- Testing/src/Calculator.java
+++ Testing/src/Calculator.java
@@ -8,4 +8,21 @@ public class Calculator {
             return DoubleStream.of(operands)
                     .reduce(1, (a, b) -> a * b);
         }
+        static String reverse_word(String word) {
+            String reversed = "";
+            for(String letter : word) {
+                reversed = letter + reversed;
+                return reversed;
+            }
+        }
+        static boolean checkPalindrome(String[] arr) {
+            if (arr[0] == reverse_word(arr[0])) {
+                if (arr[1] == reverse_word(arr[1])) {
+                    if (arr[2] == reverse_word(arr[2])) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
 }
