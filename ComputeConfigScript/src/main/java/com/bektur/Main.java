37f0dcd64275ede4799e16979e319929b9b0b0dc
Bektur13 <duyshembekov04@mail.ru>
2025-04-28 12:02:22 -0700

    Completed 90% of ComputeConfigScript

--- ComputeConfigScript/src/main/java/com/bektur/Main.java
+++ ComputeConfigScript/src/main/java/com/bektur/Main.java
@@ -9,15 +9,73 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to your config interpreter!");
-        Map<String, Integer> configs = new HashMap<>();
-        configs.put("nums_users", 100);
+        Map<String, Double> configs = new HashMap<>();
+        String[] lines = {
+                "config \"num_users\" = 100",
+                "config \"request_rate\" = 5.5",
+                "config \timeout\" = 30",
+                "update \"nums_users\" = 200",
+                "update \"request_rate\" = 7.5",
+                "compute \"total_requests\" = %num_users * %request_rate",
+                "compute \"total_timeout\" = %num_users * %timeout",
+                "show configs"
+        };
+        for(String line : lines) {
+            processLine(line, configs);
+        }
 
         System.out.println("nums_users = " + configs.get("nums_users"));
 
     }
 
-    private void processLine(String line) {
+    static void processLine(String line, Map<String, Double> configs) {
+        if(line.startsWith("config")) {
+            // Find name of variable
+            int start = line.indexOf("\"") + 1;
+            int end = line.indexOf("\"", start);
+            String name = line.substring(start, end);
 
+            // Find value of variable
+            int equalSum = line.indexOf("=");
+            String valueStr = line.substring(equalSum + 1).trim();
+            double value = Double.parseDouble(valueStr);
+
+            // Store value in Config
+            configs.put(name, value);
+
+            // Print
+            System.out.println("Added config: " + name + " = " + value);
+        }else if(line.startsWith("update")) {
+            // Find a name
+            int start = line.indexOf("\"") + 1;
+            int end = line.indexOf("\"", start);
+            String name = line.substring(start, end);
+            // Find a new value
+            int equalSum = line.indexOf("=");
+            String valueStr = line.substring(equalSum + 1).trim();
+            double updatedValue = Double.parseDouble(valueStr);
+
+            // Replace old value on updatedValue
+            configs.replace(name, updatedValue);
+
+            // Print
+            System.out.println("Updated variable: " + name + " = " + updatedValue);
+        }else if(line.startsWith("compute")) {
+            // Find a name of variable
+            int start = line.indexOf("\"") + 1;
+            int end = line.indexOf("\"", start);
+            String name = line.substring(start, end);
+
+            // Find value of variable
+            int equalSum = line.indexOf("=");
+            String expression = line.substring(equalSum + 1).trim();
+            System.out.println(expression);
+
+
+            System.out.println("This is compute line");
+        }else if(line.startsWith("show configs")) {
+            System.out.println("This is show configs line");
+        }
     }
 
 }
